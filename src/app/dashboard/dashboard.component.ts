8ccaeda5f4f8ae16b7a6e0ece77c291a63abeb75
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-23 12:56:33 +0300

    Material Admin

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,6 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
